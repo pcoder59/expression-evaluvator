44050edbc7a630c2d83e1631079ee4a64e4b3146
premraj59 <premraj59128@protonmail.com>
2021-03-13 15:39:22 +0530

    Fix Path

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', function() {
-            navigator.serviceWorker.register('/sw.js').then(function(registration) {
+            navigator.serviceWorker.register('/js/sw.js').then(function(registration) {
             // Registration was successful
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
             }, function(err) {
